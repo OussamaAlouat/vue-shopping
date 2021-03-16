f2c0a72bb8cc95596a71118830bb2c3bd8ac88fa
Oussama Alouat <al317257@uji.es>
2021-03-16 20:39:19 +0100

    Loaded data on created component and used productList view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <v-app>
+    <products-list></products-list>
   </v-app>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import ProductsList from './views/ProductsList.vue';
 
 export default {
   name: 'App',
 
   components: {
+    ProductsList,
   },
 
   data: () => ({
     //
   }),
+  created() {
+    this.initProductsAction();
+  },
+
+  methods: {
+    ...mapActions(['initProductsAction']),
+  },
 };
 </script>
