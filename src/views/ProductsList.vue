5a5bd562412179b3d4195a28cc5ae98db0b90f75
Oussama Alouat <al317257@uji.es>
2021-03-16 20:38:49 +0100

    Create productList view

---
+++ src/views/ProductsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h2> List of products </h2>
+    <div v-if="!products.length" class="spinner">
+      <v-progress-circular
+        indeterminate
+        :width="7"
+        :size="70"
+        color="primary"
+      >
+      </v-progress-circular>
+    </div>
+    <v-row v-else>
+      <div
+        v-for="product of products" :key="product.id"
+      >
+        <product
+          :product="product"
+        />
+      </div>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import Product from '../components/Product.vue';
+
+export default {
+  components: { Product },
+  computed: {
+    ...mapGetters(['products']),
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+h2 {
+  margin-bottom: 1.5rem;
+}
+.spinner {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+</style>
