c3d8c24acec109b7538064a1b5341498c35ca6e2
Oussama Alouat <al317257@uji.es>
2021-03-16 20:36:48 +0100

    Added vuex structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { getAllProducts } from '../api/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    products: [],
   },
   mutations: {
+    SET_PRODUCTS(state, data) {
+      state.products = data;
+    },
   },
   actions: {
+    initProductsAction({ commit }) {
+      getAllProducts().then((response) => {
+        commit('SET_PRODUCTS', response.data);
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
   },
-  modules: {
+  getters: {
+    products: (state) => state.products,
   },
 });
